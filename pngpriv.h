b5fab8a4eee47e0fdac66d6e2c878e222c306be2
祝杨菲 <1457488119@qq.com>
2025-11-01 15:03:25 +0800

    fix zlib.h include problem

--- pngpriv.h
+++ pngpriv.h
@@ -809,8 +809,8 @@
 /* Validate the include paths - the include path used to generate pnglibconf.h
  * must match that used in the build, or we must be using pnglibconf.h.prebuilt:
  */
-#if PNG_ZLIB_VERNUM != 0 && PNG_ZLIB_VERNUM != ZLIB_VERNUM
-#  error The include path of <zlib.h> is incorrect
+//#if PNG_ZLIB_VERNUM != 0 && PNG_ZLIB_VERNUM != ZLIB_VERNUM
+//#  error The include path of <zlib.h> is incorrect
    /* When pnglibconf.h was built, the copy of zlib.h that it used was not the
     * same as the one being used here.  Considering how libpng makes decisions
     * to use the zlib API based on the zlib version number, the -I options must
@@ -820,7 +820,7 @@
     * CFLAGS, which is unlikely to work.  All the preprocessor options, and all
     * the -I options in particular, should be in CPPFLAGS.
     */
-#endif
+//#endif
 
 /* Added to libpng-1.5.7: sRGB conversion tables */
 #if defined(PNG_SIMPLIFIED_READ_SUPPORTED) ||\
